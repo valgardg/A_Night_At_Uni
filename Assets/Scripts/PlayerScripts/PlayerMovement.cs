7d2cda288ee956299b397a401ef6c490d119aa8e
valgardg <valgardo20@ru.is>
2022-11-26 23:37:29 +0000

    added player movement and camera control

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   
+    // calls to other game objects
+    public Transform orientation;
+
+    // Player Attributes
+    public float playerSpeed = 6f;
+
+    public float groundDrag;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        MyInput();
+        SpeedControl();
+
+        rb.drag = groundDrag;
+    }
+
+    private void FixedUpdate(){
+        MovePlayer();
+    }
+
+    // Get player inputs
+    private void MyInput(){
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        // print($"horizontal: {horizontalInput} vertical: {verticalInput}");
+    }
+
+    // Calculate players movement to be applied and apply it
+    private void MovePlayer(){
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        print($"moveDirection: {moveDirection}");
+        // apply the movement
+        rb.AddForce(moveDirection.normalized * playerSpeed * 10f, ForceMode.Force);
+    }
+    
+    private void SpeedControl(){
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        if(flatVel.magnitude > playerSpeed){
+            Vector3 limitedVel = flatVel.normalized * playerSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+}
