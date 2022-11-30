23feed9c364359d2c273c87bc772c7e25e86b317
Tomasp20 <Tomasp20@ru.is>
2022-11-30 16:40:39 +0000

    added remove picked up object

---
+++ Assets/Scripts/ObjectScrips/DoorInteraction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Runtime.CompilerServices;
+using UnityEditor.UIElements;
+
+public class DoorInteraction : MonoBehaviour
+{
+    public TMP_Text text;
+    public KeyCode presskey;
+    public int rayDistance;
+    public GameObject mydoor;
+    public Animation anitmate;
+    bool doorClosed = true;
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        text.text = "";
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+
+            if (hit.collider.tag == "Door")
+            {
+                text.text = "press E to pickup " + hit.collider.tag;
+
+                if (Input.GetKeyDown(presskey))
+                {
+                    anitmate.Play();
+                    doorClosed = false;
+                    text.text = "";
+                }
+            }
+        }
+    }
+}
