1a99bbf51d6816f62103543980b747d4e8786bb8
TomasOrripet <70690540+TomasOrripet@users.noreply.github.com>
2022-12-11 20:48:42 +0000

    everything so far

--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -9,6 +10,7 @@ public class EnemyController : MonoBehaviour
     public AudioSource audio;
     public AudioSource footsteps;
     public Animator animator;
+    private bool killplaying = false;
 
     bool audioPlayed = false;
 
@@ -16,26 +18,34 @@ public class EnemyController : MonoBehaviour
 
     private void Start()
     {
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         animator.ResetTrigger("Walk");
     }
     void Update()
     {
-        if(GameManager.instance.chasePlayer && !GameManager.instance.playerHiddenState){
-            agent.SetDestination(new Vector3(player.position.x, 2, player.position.z));
-           
-            animator.SetTrigger("Walk");
-            if (audioPlayed == false){
-                //animator.SetTrigger("Walk");
-                audio.Play();
-                footsteps.Play();
-                audioPlayed = true;
+        if (!killplaying)
+        {
+            if (GameManager.instance.chasePlayer && !GameManager.instance.playerHiddenState)
+            {
+                agent.SetDestination(new Vector3(player.position.x, 2, player.position.z));
+
+                animator.SetTrigger("Walk");
+                if (audioPlayed == false)
+                {
+                    //animator.SetTrigger("Walk");
+                    audio.Play();
+                    footsteps.Play();
+                    audioPlayed = true;
+                }
             }
         }
     }
 
     private void OnTriggerEnter(Collider other){
+        killplaying = true;
+        animator.ResetTrigger("Kill");
         print("enemy hit a collider!");
-        GameManager.instance.PlayerDeath();
+        animator.SetTrigger("Kill");
+        //GameManager.instance.PlayerDeath();
     }
 }
