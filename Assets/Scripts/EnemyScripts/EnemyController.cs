23e91047205eb3c81b46421f78f35fd94575983d
TomasOrripet <70690540+TomasOrripet@users.noreply.github.com>
2022-12-11 18:06:25 +0000

    basic monster movment in alpha sceen done

--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -8,15 +8,25 @@ public class EnemyController : MonoBehaviour
     public UnityEngine.AI.NavMeshAgent agent;
     public AudioSource audio;
     public AudioSource footsteps;
+    public Animator animator;
 
     bool audioPlayed = false;
 
     // Update is called once per frame
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.ResetTrigger("Walk");
+    }
     void Update()
     {
         if(GameManager.instance.chasePlayer && !GameManager.instance.playerHiddenState){
             agent.SetDestination(new Vector3(player.position.x, 2, player.position.z));
-            if(audioPlayed == false){
+           
+            animator.SetTrigger("Walk");
+            if (audioPlayed == false){
+                //animator.SetTrigger("Walk");
                 audio.Play();
                 footsteps.Play();
                 audioPlayed = true;
