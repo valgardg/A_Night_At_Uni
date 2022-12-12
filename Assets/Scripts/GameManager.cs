331fab6558f6955293d44377a0e6b97390b57268
valgardg <valgardo20@ru.is>
2022-12-12 12:14:13 +0000

    created branch for user playability

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -95,7 +95,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         
+        instance.alive = true;
     }
 
     void FixedUpdate(){
